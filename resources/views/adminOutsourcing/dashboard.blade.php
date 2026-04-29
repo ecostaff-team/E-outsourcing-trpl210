7a3260501886b0187f2927a4682c7587a70573fe
Zhrafrdlaa <zahrahfaradila1@gmail.com>
2026-04-29 12:04:58 +0700

    Memperbaiki halaman kelola karyawan admin outsourcing

--- resources/views/adminOutsourcing/dashboard.blade.php
+++ resources/views/adminOutsourcing/dashboard.blade.php
@@ -87,6 +87,8 @@
                                 <option value="2022">2022</option>
                                 <option value="2023">2023</option>
                                 <option value="2024">2024</option>
+                                <option value="2024">2025</option>
+                                <option value="2024">2026</option>
                             </select>
                         </div>
                     </h2>
